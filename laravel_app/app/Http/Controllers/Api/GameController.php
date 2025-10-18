c4bfbc41dcdc6b111a03bd5da7af7f6e05fcafd2
funa-div <funahashi.ryouma@gmail.com>
2025-10-18 18:12:44 +0900

    一旦コミット

--- laravel_app/app/Http/Controllers/Api/GameController.php
+++ laravel_app/app/Http/Controllers/Api/GameController.php
@@ -5,11 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\RuleBook;
+use App\Models\GameLoginSession;
 
 class GameController extends Controller
 {
-    public function game_login(Request $request){
-        
+    public function game_url(Request $request){
+        $device = $request->input('device');
+        $game_type = $request->input('game_type');
+        $url = GameLoginSession::create([
+            'device' => $device,
+            'game_id' => $game_type,
+            
+        ]);
+        return response()->json([
+            'success' => true,
+            'message' => '取得に成功しました',
+            'data' => [
+                'url' => $url->token
+            ]
+        ]);
     }
     public function game_rule(Request $request, RuleBook $game_id){
         return response()->json([
